44a23f636c82487ba2cebce6e57c40633c459a1d
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-06-06 14:42:44 +0300

    Update views.py

--- smart_inventory/store/views.py
+++ smart_inventory/store/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from store.models import Product
+
+
 # Create your views here.
 def store(request):
-    context = {}
-    return render(request, 'store/store.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/store.html', context)
 
 
 def cart(request):
