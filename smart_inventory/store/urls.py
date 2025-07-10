10e142618ea6fc505a422f1dad2ea95aacf7bc69
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-07-10 16:12:40 +0300

    Update urls.py

--- smart_inventory/store/urls.py
+++ smart_inventory/store/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-
 from . import views
+
+app_name = 'store' 
 
 urlpatterns = [
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
-
     path('update_item/', views.updateItem, name="update_item"),
     path('process_order/', views.processOrder, name="process_order"),
-
+    path('category/<slug:category_slug>/', views.store, name='product_list_by_category'),
+    path('product/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
