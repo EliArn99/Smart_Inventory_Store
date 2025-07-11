8c58152d7ba783eb56dca184138f4aa908f8506f
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-07-11 17:40:47 +0300

    Update urls.py

--- smart_inventory/smart_inventory/urls.py
+++ smart_inventory/smart_inventory/urls.py
@@ -1,8 +1,8 @@
-
-"""ecommerce URL Configuration
+"""
+URL configuration for Smart_Inventory_Store project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,12 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views # For login/logout
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files (like product images) only in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
