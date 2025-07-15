ff1a482b8b82544ed45777220b8a28db95fa0ae5
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-07-15 11:58:10 +0300

    Update admin.py

--- smart_inventory/store/admin.py
+++ smart_inventory/store/admin.py
@@ -1,36 +1,79 @@
-from django.contrib import admin
-from .models import Product, Customer, Order, OrderItem, ShippingAddress
 from django.utils.html import format_html
+from django.contrib import admin
+from .models import Customer, Category, Product, Order, OrderItem, ShippingAddress, WishlistItem
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'email']
+    search_fields = ['name', 'email']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'thumbnail_image']
+    prepopulated_fields = {'slug': ('name',)}
+
+    def thumbnail_image(self, obj):
+
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="50" height="50" style="object-fit: cover; border-radius: 5px;" />'.format(
+                    obj.image.url))
+        return "-"
+
+    thumbnail_image.short_description = 'Image Preview'  #
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock_quantity', 'is_low_stock')
+    list_display = ['name', 'category', 'price', 'stock', 'available', 'digital', 'created_at', 'thumbnail_image']
+    list_filter = ['available', 'digital', 'category']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name', 'description']
 
-    def is_low_stock(self, obj):
-        return obj.stock_quantity < 5
-    is_low_stock.boolean = True
-    is_low_stock.short_description = 'Ниска наличност?'
+    def thumbnail_image(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="50" height="50" style="object-fit: cover; border-radius: 5px;" />'.format(
+                    obj.image.url))
+
+        return "-"
+
+    thumbnail_image.short_description = 'Image Preview'  #
 
 
-admin.site.register(Customer)
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', 'date_ordered', 'complete', 'transaction_id', 'shipping_status')
-    list_filter = ('complete',)
-    search_fields = ('customer__name', 'transaction_id')
+    list_display = ['id', 'customer', 'date_ordered', 'complete', 'status', 'transaction_id', 'get_cart_total',
+                    'get_cart_items']
+    list_filter = ['complete', 'status', 'date_ordered']
+    search_fields = ['customer__name', 'transaction_id']
+    inlines = [OrderItemInline]
 
-    def shipping_status(self, obj):
-        return "Изисква доставка" if obj.shipping else "Дигитален"
-    shipping_status.short_description = "Тип доставка"
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ('product', 'order', 'quantity', 'date_added')
-    search_fields = ('product__name', 'order__id')
-    list_filter = ('date_added',)
+    list_display = ['product', 'order', 'quantity', 'get_total', 'date_added']
+    list_filter = ['date_added']
+    search_fields = ['product__name', 'order__id']
+
 
 @admin.register(ShippingAddress)
 class ShippingAddressAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'order', 'city', 'state', 'zipcode', 'date_added')
-    search_fields = ('customer__name', 'order__id', 'address')
+    list_display = ['customer', 'order', 'address', 'city', 'state', 'zipcode', 'date_added']
+    list_filter = ['date_added', 'city']
+    search_fields = ['customer__name', 'order__id', 'address', 'city']
 
+
+@admin.register(WishlistItem)
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'product']
+    list_filter = ['customer', 'product']
+    search_fields = ['customer__name', 'product__name']
