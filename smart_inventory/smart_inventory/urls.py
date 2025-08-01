42ba668a97dd62c90ef8f93b87cc4eba398ca2b7
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-08-01 18:56:16 +0300

    Update urls.py

--- smart_inventory/smart_inventory/urls.py
+++ smart_inventory/smart_inventory/urls.py
@@ -1,3 +1,5 @@
+# Smart_Inventory_Store/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views  # Import Django's built-in auth views
@@ -7,16 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
 
-    # --- Authentication URLs ---
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('register/', store_views.register_user, name='register'),
-
-    # --- User Profile URL ---
-    path('profile/', store_views.user_profile, name='profile'),  # User profile page
 ]
 
-# Optional: Serve media files during development (add only in development)
 from django.conf import settings
 from django.conf.urls.static import static
 
