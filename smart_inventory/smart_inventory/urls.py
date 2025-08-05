1d8eb4ea438432bffb6cb080d2cc9e1a3256fa67
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-08-05 14:26:55 +0300

    Update urls.py

--- smart_inventory/smart_inventory/urls.py
+++ smart_inventory/smart_inventory/urls.py
@@ -1,10 +1,5 @@
-# Smart_Inventory_Store/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views  # Import Django's built-in auth views
-from store import views as store_views  # Import views from your store app
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
