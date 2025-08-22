dbd2446844e4dbbc6a7dbc0cc3e665dde9cfb342
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-08-22 16:47:41 +0300

    Update urls.py

--- smart_inventory/store/urls.py
+++ smart_inventory/store/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'store' 
+app_name = 'store'
 
 urlpatterns = [
     path('', views.store, name="store"),
@@ -16,6 +16,10 @@ urlpatterns = [
     path('search/', views.search_results, name='search_results'),
 
     path('update_wishlist/', views.update_wishlist, name='update_wishlist'),
+
+    # path('', views.post_list, name='post_list'),
+    #
+    # path('<slug:slug>/', views.post_detail, name='post_detail'),
 
     # AJAX endpoint for updating cart items (add/remove)
     path('update_item/', views.updateItem, name="update_item"),
