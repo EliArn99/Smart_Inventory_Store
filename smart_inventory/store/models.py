66fbc714eb127a1a4910a6cd2bdd60eaf936cfc3
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-08-22 16:47:28 +0300

    Update models.py

--- smart_inventory/store/models.py
+++ smart_inventory/store/models.py
@@ -145,3 +145,20 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Ревю от {self.user.username} за {self.book.name}"
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=[(0, "Draft"), (1, "Published")] ,default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
