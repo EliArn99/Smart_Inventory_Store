9862ea87db87286943293b91486ef7398d104bf1
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-09-02 17:46:17 +0300

    Update forms.py

--- smart_inventory/store/forms.py
+++ smart_inventory/store/forms.py
@@ -1,10 +1,8 @@
-# store/forms.py
-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from store.models import Review
+from store.models import Review, Post, Comment
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -20,4 +18,26 @@ class ReviewForm(forms.ModelForm):
         widgets = {
             'rating': forms.Select(choices=[(i, str(i)) for i in range(1, 6)]),
             'comment': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Оставете коментар тук...'}),
+        }
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'status']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-select'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+        }
+        labels = {
+            'content': 'Вашият коментар',
         }
