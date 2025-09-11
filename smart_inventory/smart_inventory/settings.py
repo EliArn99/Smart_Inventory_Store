5099f1bc10e69993656157b9ff70129b4d27efed
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-09-11 13:35:34 +0300

    Update settings.py

--- smart_inventory/smart_inventory/settings.py
+++ smart_inventory/smart_inventory/settings.py
@@ -131,6 +131,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+
+
 LOGIN_REDIRECT_URL = 'store:store'
 LOGOUT_REDIRECT_URL = 'store:store'
 
