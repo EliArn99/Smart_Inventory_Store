6a5a1944058fbcc5a9524cbfcbf4c7e9b4cadeba
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-09-11 13:35:52 +0300

    Update urls.py

--- smart_inventory/smart_inventory/urls.py
+++ smart_inventory/smart_inventory/urls.py
@@ -1,13 +1,19 @@
-
 from django.contrib import admin
+
 from django.urls import path, include
+
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+
+    path('accounts/', include('django.contrib.auth.urls')),
+
 
 ]
 
 from django.conf import settings
+
 from django.conf.urls.static import static
 
 if settings.DEBUG:
