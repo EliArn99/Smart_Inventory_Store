28186813ad8542c047e846ed6344e730b94cc3a1
Eli Arnaytska <142402112+EliArn99@users.noreply.github.com>
2025-09-13 16:01:23 +0300

    Update signals.py

--- smart_inventory/store/signals.py
+++ smart_inventory/store/signals.py
@@ -1,66 +1,34 @@
-# from django.apps import AppConfig
-# from django.core.mail import send_mail
-# from django.db.models.signals import post_save
-# from django.contrib.auth.models import User
-# from django.dispatch import receiver
-#
-# from django.conf import settings
-# from .models import Customer, Book
-#
-#
-# @receiver(post_save, sender=User)
-# def create_customer_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Customer.objects.create(user=instance, name=instance.username)
-#
-# class StoreConfig(AppConfig):
-#     # ...
-#     def ready(self):
-#         import store.signals
-#
-#
-# @receiver(post_save, sender=Book)
-# def check_low_stock(sender, instance, created, **kwargs):
-#     LOW_STOCK_THRESHOLD = 5
-#
-#     if not created and instance.stock <= LOW_STOCK_THRESHOLD and instance.stock > 0:
-#         subject = f'Предупреждение за ниска наличност: {instance.name}'
-#         message = (
-#             f'Наличността на книгата "{instance.name}" от автор {instance.author} '
-#             f'е намаляла до {instance.stock} бройки.\n\n'
-#             f'Моля, поръчайте нови количества.'
-#         )
-#
-#
-#         recipient_list = ['your_email@example.com']
-#
-#         try:
-#             send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
-#         except Exception as e:
-#             print(f"Грешка при изпращане на имейл: {e}")
-
-
-
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.models import User
-
 from .models import Customer, Book
+
+
+_old_stock = {}
+
+@receiver(pre_save, sender=Book)
+def pre_save_book(sender, instance, **kwargs):
+    if instance.pk:
+        _old_stock[instance.pk] = Book.objects.get(pk=instance.pk).stock
+
 
 @receiver(post_save, sender=User)
 def create_customer_profile(sender, instance, created, **kwargs):
     if created:
-        Customer.objects.create(user=instance, name=instance.username)
+        Customer.objects.create(user=instance, name=instance.username, email=instance.email)
 
 
 @receiver(post_save, sender=Book)
 def check_low_stock(sender, instance, created, **kwargs):
+    if not hasattr(settings, 'LOW_STOCK_ALERT_EMAIL'):
+        return
+
     LOW_STOCK_THRESHOLD = 5
+    old_stock = _old_stock.get(instance.pk, instance.stock + 1)
 
-
-    if not created and instance.stock <= LOW_STOCK_THRESHOLD and instance.stock > 0:
+    if not created and old_stock > LOW_STOCK_THRESHOLD and instance.stock <= LOW_STOCK_THRESHOLD:
         subject = f'Предупреждение за ниска наличност: {instance.name}'
         message = (
             f'Наличността на книгата "{instance.name}" от автор {instance.author} '
@@ -68,9 +36,7 @@ def check_low_stock(sender, instance, created, **kwargs):
             f'Моля, поръчайте нови количества.'
         )
 
-        #TODO: Fix here
-        # Замени с твоя имейл
-        recipient_list = ['eli_arnaytska@abv,bg']
+        recipient_list = [settings.LOW_STOCK_ALERT_EMAIL]
 
         try:
             send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list, fail_silently=False)
