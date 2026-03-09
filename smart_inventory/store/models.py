bc4287cd4137c1d75a334d10703c560f4095ef04
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-03-09 18:47:50 +0200

    Update models.py

--- smart_inventory/store/models.py
+++ smart_inventory/store/models.py
@@ -59,7 +59,7 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
-    transaction_id = models.CharField(max_length=100, null=True)
+    transaction_id = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return str(self.id)
