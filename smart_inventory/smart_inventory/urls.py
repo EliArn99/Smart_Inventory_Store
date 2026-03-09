dd16e8c86c5f9943c18daa4dec610e4f8737bd88
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-03-09 18:46:12 +0200

    Update urls.py

--- smart_inventory/smart_inventory/urls.py
+++ smart_inventory/smart_inventory/urls.py
@@ -1,20 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-
-    path('admin/', admin.site.urls),
-    path('store/', include('store.urls')),
-
-    path('accounts/', include('django.contrib.auth.urls')),
-
-
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("store/", include("store.urls")),
 ]
-
-from django.conf import settings
-
-from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
