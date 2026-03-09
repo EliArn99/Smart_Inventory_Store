e66ffc1a1818be190b4d03f71a5dd64661ebd213
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-03-09 18:46:31 +0200

    Update asgi.py

--- smart_inventory/smart_inventory/asgi.py
+++ smart_inventory/smart_inventory/asgi.py
@@ -1,16 +1,9 @@
-"""
-ASGI config for smart_inventory project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_inventory.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    os.getenv("DJANGO_SETTINGS_MODULE", "Smart_Inventory_Store.settings.prod")
+)
 
 application = get_asgi_application()
