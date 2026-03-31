aaf672d9e4d24011660fc26b5d72ac586f66d3f6
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-03-31 10:13:54 +0300

    Update cart.js

--- smart_inventory/static/js/cart/cart.js
+++ smart_inventory/static/js/cart/cart.js
@@ -1,13 +1,15 @@
-import {postJson} from "../core/api.js";
-import {updateCartBadge} from "../ui/cartBadge.js";
+import { postJson } from "../core/api.js";
+import { updateCartBadge } from "../ui/cartBadge.js";
 
 export async function updateCart(productId, action, csrfToken) {
-
-    const data = await postJson(
-        "/store/update_item/",
-        {bookId: productId, action},
-        csrfToken
-    );
-
-    updateCartBadge(data.cartItems);
+    try {
+        const data = await postJson(
+            "/store/update_item/",
+            { bookId: productId, action },
+            csrfToken
+        );
+        updateCartBadge(data.cartItems);
+    } catch (error) {
+        console.error("Cart error:", error);
+    }
 }
