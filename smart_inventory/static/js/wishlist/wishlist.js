92a8d809093101e893b6aa3740953ef308b52096
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-03-31 10:15:22 +0300

    Refactor updateWishlist function formatting

--- smart_inventory/static/js/wishlist/wishlist.js
+++ smart_inventory/static/js/wishlist/wishlist.js
@@ -1,10 +1,9 @@
-import {postJson} from "../core/api.js";
+import { postJson } from "../core/api.js";
 
 export async function updateWishlist(bookId, action, csrfToken) {
-
     return await postJson(
         "/store/update_wishlist/",
-        {bookId, action},
+        { bookId, action },
         csrfToken
     );
 }
