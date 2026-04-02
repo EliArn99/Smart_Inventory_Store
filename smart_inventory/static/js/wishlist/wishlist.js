87ef576739dcd840f066b5d4bd4b298ef6a27d33
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-04-02 16:18:10 +0300

    Delete smart_inventory/static/js/wishlist directory

--- smart_inventory/static/js/wishlist/wishlist.js
+++
@@ -1,9 +0,0 @@
-import { postJson } from "../core/api.js";
-
-export async function updateWishlist(bookId, action, csrfToken) {
-    return await postJson(
-        "/store/update_wishlist/",
-        { bookId, action },
-        csrfToken
-    );
-}
