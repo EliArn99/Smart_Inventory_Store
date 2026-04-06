113e7aa828f91a75ca2bceaa136ea4a2a263e228
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-04-06 17:08:28 +0300

    Update cart.js

--- smart_inventory/static/js/cart/cart.js
+++ smart_inventory/static/js/cart/cart.js
@@ -8,8 +8,18 @@ export async function updateCart(productId, action, csrfToken) {
             { bookId: productId, action },
             csrfToken
         );
+
+        console.log("Server response:", data);
+
         updateCartBadge(data.cartItems);
+
+        if (window.location.pathname.includes("/cart")) {
+            window.location.reload();
+        }
+
+        return data;
     } catch (error) {
         console.error("Cart error:", error);
+        return null;
     }
 }
