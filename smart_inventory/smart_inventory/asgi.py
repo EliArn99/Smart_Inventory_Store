5acdbc8c228bdc39ab601f4cda57cd06b3b09dd2
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-04-27 18:02:57 +0300

    Update asgi.py

--- smart_inventory/smart_inventory/asgi.py
+++ smart_inventory/smart_inventory/asgi.py
@@ -1,9 +1,11 @@
 import os
 from django.core.asgi import get_asgi_application
 
+env = os.getenv("DJANGO_ENV", "prod").lower()
+
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    os.getenv("DJANGO_SETTINGS_MODULE", "Smart_Inventory_Store.settings.prod")
+    f"Smart_Inventory_Store.settings.{env}"
 )
 
 application = get_asgi_application()
