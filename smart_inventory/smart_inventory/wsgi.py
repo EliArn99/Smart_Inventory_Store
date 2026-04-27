0bc7c6152ee25e2a037f2f12c3f69c89b55f5d60
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-04-27 18:03:54 +0300

    Update wsgi.py

--- smart_inventory/smart_inventory/wsgi.py
+++ smart_inventory/smart_inventory/wsgi.py
@@ -1,9 +1,11 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+env = os.getenv("DJANGO_ENV", "prod").lower()
+
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    os.getenv("DJANGO_SETTINGS_MODULE", "Smart_Inventory_Store.settings.prod")
+    f"Smart_Inventory_Store.settings.{env}"
 )
 
 application = get_wsgi_application()
