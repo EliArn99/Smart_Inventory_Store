b1e2d973bbd62225587b024c35017bd6996eb0e4
Eli Arnaytska <eli_arnaytska@abv.bg>
2026-05-05 18:02:27 +0300

    Update prod.py

--- smart_inventory/smart_inventory/settings/prod.py
+++ smart_inventory/smart_inventory/settings/prod.py
@@ -22,6 +22,8 @@ SECURE_HSTS_SECONDS = int(get_env("DJANGO_SECURE_HSTS_SECONDS", 31536000))
 SECURE_HSTS_INCLUDE_SUBDOMAINS = get_bool_env("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", True)
 SECURE_HSTS_PRELOAD = get_bool_env("DJANGO_SECURE_HSTS_PRELOAD", True)
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 EMAIL_BACKEND = get_env(
     "EMAIL_BACKEND",
     "django.core.mail.backends.smtp.EmailBackend",
